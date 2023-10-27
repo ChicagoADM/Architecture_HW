3081f2964d243dcf80c865d6907af4b93269e844
4icag0 <112687685+ChicagoADM@users.noreply.github.com>
2023-10-27 09:30:13 +0300

    HW_04

---
+++ HW_04/core/cashProvider.py
@@ -0,0 +1,18 @@
+from HW_04.models.ticket import Ticket
+from HW_04.services.bancRepo import BancRepo
+class CashProvider:
+    def __init__(self):
+        self.bancRepo = BancRepo()
+        self.cardNumber = 0
+        self.isAuthorized = False
+
+    def buy(self, ticket: Ticket):
+        if self.isAuthorized:
+            return self.bancRepo.request(ticket.getPrice(), self.cardNumber)
+        return False
+
+    def authorization(self, client):
+        self.cardNumber = client.getCardNumber()
+        self.isAuthorized = True
+
+        print(self.cardNumber, self.isAuthorized )
